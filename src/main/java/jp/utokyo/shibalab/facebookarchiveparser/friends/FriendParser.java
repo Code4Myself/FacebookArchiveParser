bc86f7809cb1cf6cac91cf15c0d66541aee4a1aa
knsg <yok@csis.u-tokyo.ac.jp>
2018-07-07 01:51:29 +0900

    update

--- src/main/java/jp/utokyo/shibalab/facebookarchiveparser/friends/FriendParser.java
+++ src/main/java/jp/utokyo/shibalab/facebookarchiveparser/friends/FriendParser.java
@@ -3,7 +3,9 @@ package jp.utokyo.shibalab.facebookarchiveparser.friends;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -20,7 +22,7 @@ public class FriendParser {
 	 * static fields
 	 * ============================================================== */
 	/** friends.json: friends list file in JSON format */
-	public static final String FRIEND_JSON = "friends.json";
+	public static final String FRIENDS_JSON                  = "friends.json";
 	
 	/** received_friend_request.json: received friend request list in JSON format */
 	public static final String RECEIVED_FRIEND_REQUESTS_JSON = "received_friend_requests.json";
@@ -39,35 +41,73 @@ public class FriendParser {
 	/* ==============================================================
 	 * instance methods
 	 * ============================================================== */
-
-
+	/**
+	 * extract all friend JSON data
+	 * @param friendsDir friend directory path
+	 * @return friend data list (key=friend type, value=friend list)
+	 */
+	public Map<String,List<Friend>> parseAllFriends(File friendsDir) {
+		Map<String,List<Friend>> friendMap = new HashMap<>();
+		{
+			friendMap.put(FRIENDS_JSON,                  parseFriends(         new File(friendsDir,FRIENDS_JSON))                 );
+			friendMap.put(RECEIVED_FRIEND_REQUESTS_JSON, parseReceivedRequests(new File(friendsDir,RECEIVED_FRIEND_REQUESTS_JSON)));
+			friendMap.put(REJECTED_FRIEND_REQUESTS_JSON, parseRejectedRequests(new File(friendsDir,REJECTED_FRIEND_REQUESTS_JSON)));
+			friendMap.put(REMOVED_FRIENDS_JSON,          parseRemovedFriends(  new File(friendsDir,REMOVED_FRIENDS_JSON))         );
+			friendMap.put(SENT_FRIEND_REQUESTS_JSON,     parseSentRequests(    new File(friendsDir,SENT_FRIEND_REQUESTS_JSON))    );
+		}
+		return friendMap;
+	}
 	
-	public List<Friend> parseFriend(File jsonFile) {
+	/**
+	 * parse friend JSON data 
+	 * @param jsonFile JSON file
+	 * @return friend list 
+	 */
+	public List<Friend> parseFriends(File jsonFile) {
 		return parseFriends(jsonFile,Friends.class);
 	}
 	
+	/**
+	 * parse friend JSON data of removed friends
+	 * @param jsonFile JSON file
+	 * @return friend list 
+	 */
 	public List<Friend> parseRemovedFriends(File jsonFile) {
 		return parseFriends(jsonFile,RemovedFriends.class);
 	}
 
+	/**
+	 * parse friend JSON data of rejected request
+	 * @param jsonFile JSON file
+	 * @return friend list 
+	 */
 	public List<Friend> parseRejectedRequests(File jsonFile) {
 		return parseFriends(jsonFile,RejectedRequests.class);
 	}
 
+	/**
+	 * parse friend JSON data of received request
+	 * @param jsonFile JSON file
+	 * @return friend list 
+	 */
 	public List<Friend> parseReceivedRequests(File jsonFile) {
 		return parseFriends(jsonFile,ReceivedRequests.class);
 	}
 
+	/**
+	 * parse friend JSON data of sent request 
+	 * @param jsonFile JSON file
+	 * @return friend list 
+	 */
 	public List<Friend> parseSentRequests(File jsonFile) {
 		return parseFriends(jsonFile,SentRequests.class);
 	}
-
 	
 	/**
-	 * 
-	 * @param jsonFile
-	 * @param clazz
-	 * @return
+	 * parse friend JSON
+	 * @param jsonFile JSON file
+	 * @param clazz friend class
+	 * @return friend list
 	 */
 	private List<Friend> parseFriends(File jsonFile, Class<? extends AFriends> clazz) {
 		// load configuration JSON file ///////////////////
@@ -94,5 +134,4 @@ public class FriendParser {
 		}
 		return null;
 	}
-	
 }
